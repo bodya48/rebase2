0cce29662056c2ac0f3aca7cdab58417d13ac23d
Bogdan Laukhin <blaukhin@plexteq.com>
2019-08-22 17:24:45 +0300

    added class for develop branch

---
+++ rebase2/ClassForDevelopBranch.h
@@ -0,0 +1,19 @@
+//
+//  ClassForDevelopBranch.h
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClassForDevelopBranch : NSObject
+
+- (void)methodForDevelop;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rebase2/ClassForDevelopBranch.m
@@ -0,0 +1,17 @@
+//
+//  ClassForDevelopBranch.m
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import "ClassForDevelopBranch.h"
+
+@implementation ClassForDevelopBranch
+
+- (void)methodForDevelop {
+    
+}
+
+@end
