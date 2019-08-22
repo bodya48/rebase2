0b3e1dc7d2bc553d6af83f0e7c4f17126895468b
Bogdan Laukhin <blaukhin@plexteq.com>
2019-08-22 15:17:22 +0300

    added feature 2 class

---
+++ rebase2/feature2Class.h
@@ -0,0 +1,17 @@
+//
+//  feature2Class.h
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface feature2Class : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rebase2/feature2Class.m
@@ -0,0 +1,13 @@
+//
+//  feature2Class.m
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import "feature2Class.h"
+
+@implementation feature2Class
+
+@end
