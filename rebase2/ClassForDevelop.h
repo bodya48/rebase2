4bae28ce8dc5573fa306b5cc7be05d0ae6de4df7
Bogdan Laukhin <blaukhin@axcient.com>
2019-08-22 20:04:33 +0300

    added classes for develop

---
+++ rebase2/ClassForDevelop.h
@@ -0,0 +1,17 @@
+//
+//  ClassForDevelop.h
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClassForDevelop : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rebase2/ClassForDevelop.m
@@ -0,0 +1,13 @@
+//
+//  ClassForDevelop.m
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import "ClassForDevelop.h"
+
+@implementation ClassForDevelop
+
+@end
