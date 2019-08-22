cdb1761ede9798dea5287009650d6e4868dd932f
Bogdan Laukhin <blaukhin@plexteq.com>
2019-08-22 17:26:17 +0300

    added methods for feature branch

---
+++ rebase2/ClassForFeatureBranch.h
@@ -0,0 +1,19 @@
+//
+//  ClassForFeatureBranch.h
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClassForFeatureBranch : NSObject
+
+- (void)forFeatureBranch;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rebase2/ClassForFeatureBranch.m
@@ -0,0 +1,17 @@
+//
+//  ClassForFeatureBranch.m
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import "ClassForFeatureBranch.h"
+
+@implementation ClassForFeatureBranch
+
+- (void)forFeatureBranch {
+    
+}
+
+@end
