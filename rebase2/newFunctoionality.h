a5a938c9e53670d59bc380dbaade088ffee3bcd7
Bogdan Laukhin <blaukhin@plexteq.com>
2019-08-22 15:24:54 +0300

    added new functionality on develop

---
+++ rebase2/newFunctoionality.h
@@ -0,0 +1,17 @@
+//
+//  newFunctoionality.h
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface newFunctoionality : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rebase2/newFunctoionality.m
@@ -0,0 +1,13 @@
+//
+//  newFunctoionality.m
+//  rebase2
+//
+//  Created by Bogdan Laukhin on 8/22/19.
+//  Copyright © 2019 Bogdan Laukhin. All rights reserved.
+//
+
+#import "newFunctoionality.h"
+
+@implementation newFunctoionality
+
+@end
